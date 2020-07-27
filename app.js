805d0c96d79393b6d9666fea314ebdff2cfd0ea0
Juyeon <remake867@gmail.com>
2020-07-27 22:27:06 +0900

    NS005-5 unread count CRUD

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var keywordRouter = require('./routes/keyword');
+var unreadCountRouter = require('./routes/unread_count');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/keyword', keywordRouter);
+app.use('/unread-count', unreadCountRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
