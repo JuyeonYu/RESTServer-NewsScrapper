5dff114ee3ad0ab9b96709304f9da53ffb015d4e
JuyeonYu <remake111@ucware.onmicrosoft.com>
2020-07-27 16:29:21 +0900

    NS005-2 user crud

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql_dbc = require('../db/db_con')();
+var connection = mysql_dbc.init();
+
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res, next) {
+  // var param = req.param('userid');
+  var param = req.params.id;
+  var sql = 'SELECT * from user where user_id = ?';
+  connection.query(sql, param, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        res.send(rows[0]);
+    });
+});
+
+router.post('/', function(req, res, next) {
+  var userid = req.body.userid;
+  var token = req.body.token;
+  var params = [userid, token]
+
+  var sql = 'insert into user(user_id, push_token) value(?,?)';
+  
+  connection.query(sql, params, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        else console.log(rows)
+        res.send(rows);
+    });
+});
+
+router.put('/:id', function(req, res, next) {
+  var token = req.body.token;
+  var id = req.params.id;
+  var params = [token, id];
+
+  var sql = 'update user set push_token = ? where user_id = ?';
+  
+  connection.query(sql, params, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        else console.log(rows)
+        res.send(rows);
+    });
+});
+
+router.delete('/:id', function(req, res, next) {
+  // var param = req.param('userid');
+  var param = req.params.id;
+  var sql = 'delete from user where user_id = ?';
+  connection.query(sql, param, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        res.send(rows[0]);
+    });
 });
 
 module.exports = router;
 
 
-var mysql_dbc = require('../db/db_con')();
-var connection = mysql_dbc.init();
+
 mysql_dbc.test_open(connection);
