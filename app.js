9897fa4b5d872a91871e59e2e1f8b90d86d92a56
Juyeon <remake867@gmail.com>
2020-08-04 00:51:54 +0900

    up

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var keywordRouter = require('./routes/keyword');
-var unreadCountRouter = require('./routes/unread_count');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/keyword', keywordRouter);
-app.use('/unread-count', unreadCountRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/user.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql_dbc = require('../db/db_con')();
+var connection = mysql_dbc.init();
+
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+/* GET users listing. */
+router.get('/:id', function(req, res, next) {
+  // var param = req.param('userid');
+  var param = req.params.id;
+  var sql = 'SELECT * from user where user_id = ?';
+  connection.query(sql, param, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        res.send(rows[0]);
+    });
+});
+
+router.post('/', function(req, res, next) {
+  var userid = req.body.userID;
+  var token = req.body.pushToken;
+  var params = [userid, token, token];
+
+  console.log('userid: ' + userid + '/ token: ' + token + '/');
+
+  var sql = 'insert into user(user_id, push_token) value(?,?) ON DUPLICATE KEY UPDATE push_token=?';
+  
+  connection.query(sql, params, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        else console.log(rows)
+        res.send(rows);
+    });
+});
+
+router.put('/:id', function(req, res, next) {
+  var token = req.body.token;
+  var id = req.params.id;
+  var params = [token, id];
+
+  var sql = 'update user set push_token = ? where user_id = ?';
+  
+  connection.query(sql, params, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        else console.log(rows)
+        res.send(rows);
+    });
+});
+
+router.delete('/:id', function(req, res, next) {
+  // var param = req.param('userid');
+  var param = req.params.id;
+  var sql = 'delete from user where user_id = ?';
+  connection.query(sql, param, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        res.send(rows[0]);
+    });
+});
+
+module.exports = router;
+
+
+
+mysql_dbc.test_open(connection);
